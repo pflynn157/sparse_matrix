055d50b5633f8e27ad6c45822657542638486e70
Patrick Flynn <pflynn157@gmail.com>
2023-10-31 09:46:37 -0400

    Successful BCSR loading

--- py-load/load.py
+++ py-load/load.py
@@ -103,6 +103,15 @@ def generate_ellpack(coo):
 ##
 ## Generates a BCSR matrix and loads its
 ##
+def has_values(i, mi, j, mj, coo):
+    for bi in range(i, mi):
+        for bj in range(j, mj):
+            
+            for coord in coo:
+                if coord[0] == bi and coord[1] == bj:
+                    return True
+    return False
+
 def generate_bcsr(coo):
     size = coo[0]
     rows = size[0]
@@ -110,7 +119,9 @@ def generate_bcsr(coo):
     coo = coo[1:]
     print(coo)
     
-    coo2 = list()
+    A2pos_nc = list()
+    A2crd = list()
+    Aval = list()
     
     # Step 1: Determine block size
     # TODO: Let us think about this. For now, quick solution
@@ -125,22 +136,62 @@ def generate_bcsr(coo):
     #
     for i in range(0, rows, block_rows):
         for j in range(0, cols, block_cols):
-        
-            for bi in range(i, i+block_rows):
-                print("-bi: ", bi)
+            print("I: ", i, " | J: ", j)
             
-            #for j in range(0, cols, block_cols):
-                for bj in range(j, j+block_cols):
-                    print("bj: ", bj)
-                print("--")
+            # Note: for A2_crd, corresponds to j, divide by "block_cols"
+            # to get the block position
             
-        # Check and see if this area needs padding
+            # Check the block and see if it has values
+            # If so, we use it
+            if has_values(i, i+block_rows, j, j+block_cols, coo):
+                print("--Block has values")
+                A2pos_nc.append(int(i/block_rows))
+                A2crd.append(int(j/block_cols))
+                
+                # Add all the values including the padded zeros
+                #
+                # To do this, we first search the COO array for a non-zero
+                # value. If there is no such value, then we add the current
+                # index with a zero.
+                #
+                for bi in range(i, i+block_rows):
+                    for bj in range(j, j+block_cols):
+                        found = False
+                        for coord in coo:
+                            if coord[0] == bi and coord[1] == bj:
+                                Aval.append(coord[2])
+                                found = True
+                                break
+                        if not found:
+                            Aval.append(0)
+            
+            # Debug (for now)
+            #for bi in range(i, i+block_rows):
+            #    print("-bi: ", bi)
+            #    for bj in range(j, j+block_cols):
+            #        print("bj: ", bj)
+            #    print("--")
         
         
             print("----")
+    
+    # Compress the row coordinates
+    A2pos = [0]
+    
+    current = A2pos_nc[0]
+    current_end = 1
+    for i in range(1, len(A2pos_nc)):
+        if A2pos_nc[i] != current:
+            A2pos.append(current_end)
+            current = A2pos_nc[i]
+        current_end += 1
+    A2pos.append(current_end)
+    
         
     # Print the final
-    print(coo2)
+    print("A2pos: ", A2pos)
+    print("A2crd: ", A2crd)
+    print("Aval: ", Aval)
 
 ## Run the generator
 #generate_ellpack(coo)
