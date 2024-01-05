1214685579c0c59d4b65ff1416d51595e87af7e7
Patrick Flynn <pflynn157@gmail.com>
2024-01-04 20:12:10 -0500

    New MM benchmarks

---
+++ comet-mm-bench2/run.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+if [ ! -d build ]
+then
+    mkdir build
+fi
+
+if [ ! -d csv ]
+then
+    mkdir csv
+    mkdir csv/CSR
+    mkdir csv/COO
+    mkdir csv/ELL
+    mkdir csv/BCSR
+fi
+
+#export LD_LIBRARY_PATH="/home/patrick/Work/PNNL/COMET/install/lib"
+#export COMET_PATH="/home/patrick/Work/PNNL/COMET/build"
+#export LLVM_PATH="/home/patrick/Work/PNNL/COMET/llvm/build/bin"
+export LD_LIBRARY_PATH="/home/pflynn5/COMET/install/lib"
+export COMET_PATH="/home/pflynn5/COMET/build"
+export LLVM_PATH="/home/pflynn5/COMET/llvm/build/bin"
+
+##
+## Depends on environment variables being set
+## SPARSE_FILE_NAME0
+## $1 = the name for our file
+##
+function run_benchmark() {
+    # CSR
+    printf "" > csv/CSR/CSR_$1.csv
+    echo "Running $1 for CSR"
+    $LLVM_PATH/mlir-cpu-runner $1/csr_$1.mlir -O3 -e main -entry-point-result=void \
+        -shared-libs=$COMET_PATH/lib/libcomet_runner_utils.so | grep -oP '\d+\.\d+' >> csv/CSR/CSR_$1.csv 
+    
+    # COO
+    printf "" > csv/COO/COO_$1.csv
+    echo "Running $1 for COO"
+    $LLVM_PATH/mlir-cpu-runner $1/coo_$1.mlir -O3 -e main -entry-point-result=void \
+        -shared-libs=$COMET_PATH/lib/libcomet_runner_utils.so | grep -oP '\d+\.\d+' >> csv/COO/COO_$1.csv
+    
+    # ELL
+    printf "" > csv/ELL/ELL_$1.csv
+    echo "Running $1 for ELL"
+    $LLVM_PATH/mlir-cpu-runner $1/ell_$1.mlir -O3 -e main -entry-point-result=void \
+        -shared-libs=$COMET_PATH/lib/libcomet_runner_utils.so | grep -oP '\d+\.\d+' >> csv/ELL/ELL_$1.csv
+    
+    # BCSR
+    # Block sizes 2, 4, 8, 16, 32, default
+    export BLOCK_ROWS=1
+    export BLOCK_COLS=1
+    echo "Running $1 for BCSR 1x1"
+    printf "" > csv/BCSR/BCSR_$1_2x2.csv
+    $LLVM_PATH/mlir-cpu-runner $1/bcsr_$1.mlir -O3 -e main -entry-point-result=void \
+        -shared-libs=$COMET_PATH/lib/libcomet_runner_utils.so | grep -oP '\d+\.\d+' >> csv/BCSR/BCSR_$1_2x2.csv
+   
+}
+
+export SPARSE_FILE_NAME0=../data/data/bcsstk17/bcsstk17.mtx
+run_benchmark "bcsstk17"
+
+#export SPARSE_FILE_NAME0=../data/data/cant/cant.mtx
+#run_benchmark "cant"
+
+# Only need BCSR default
+#export SPARSE_FILE_NAME0=../data/data/consph/consph.mtx
+#run_benchmark "consph"
+
+#export SPARSE_FILE_NAME0=../data/data/cop20k_A/cop20k_A.mtx
+#run_benchmark "cop20k_A"
+
+#export SPARSE_FILE_NAME0=../data/data/pdb1HYS/pdb1HYS.mtx
+#run_benchmark "pdb1HYS"
+#
+#export SPARSE_FILE_NAME0=../data/data/rma10/rma10.mtx
+#run_benchmark "rma10"
+#
+#export SPARSE_FILE_NAME0=../data/data/rma10/rma10_b.mtx
+#run_benchmark "rma10_b"
+
+#export SPARSE_FILE_NAME0=../data/data/scircuit/scircuit.mtx
+#run_benchmark "scircuit"
+
+#export SPARSE_FILE_NAME0=../data/data/scircuit/scircuit_b.mtx
+#run_benchmark "scircuit_b"
+
+#export SPARSE_FILE_NAME0=../data/data/shipsec1/shipsec1.mtx
+#run_benchmark "shipsec1"
+
+
+# com-LiveJournal
+#export SPARSE_FILE_NAME0=../data/data/com-LiveJournal/com-LiveJournal.mtx
+#run_benchmark "com-LiveJournal"
+
+#export SPARSE_FILE_NAME0=../data/data/com-LiveJournal/com-LiveJournal_Communities_all.mtx
+#run_benchmark "com-LiveJournal_Communities_all"
+
+#export SPARSE_FILE_NAME0=../data/data/com-LiveJournal/com-LiveJournal_Communities_top5000.mtx
+#run_benchmark "com-LiveJournal_Communities_top5000"
+
+#export SPARSE_FILE_NAME0=../data/data/com-LiveJournal/com-LiveJournal_nodeid.mtx
+#run_benchmark "com-LiveJournal_nodeid"
+
+
+# com-Orkut
+#export SPARSE_FILE_NAME0=../data/data/com-Orkut/com-Orkut.mtx
+#run_benchmark "com-Orkut"
+
+#export SPARSE_FILE_NAME0=../data/data/com-Orkut/com-Orkut_Communities_all.mtx
+#run_benchmark "com-Orkut_Communities_all"
+
+#export SPARSE_FILE_NAME0=../data/data/com-Orkut/com-Orkut_Communities_top5000.mtx
+#run_benchmark "com-Orkut_Communities_top5000"
+
+#export SPARSE_FILE_NAME0=../data/data/com-Orkut/com-Orkut_nodeid.mtx
+#run_benchmark "com-Orkut_nodeid"
+
