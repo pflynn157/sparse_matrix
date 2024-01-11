94b9e14fe1266894056ba14e18114a8a27ee62a7
Patrick Flynn <pflynn157@gmail.com>
2024-01-10 20:01:24 -0500

    Setup for final tests

--- comet-mm-bench-ellpack/run.sh
+++ comet-mm-bench-ellpack/run.sh
@@ -8,18 +8,16 @@ fi
 if [ ! -d csv ]
 then
     mkdir csv
-    mkdir csv/CSR
-    mkdir csv/COO
-    mkdir csv/ELL
-    mkdir csv/BCSR
 fi
 
-#export LD_LIBRARY_PATH="/home/patrick/Work/PNNL/COMET/install/lib"
+#export LD_LIBRARY_PATH="/home/patrick/Work/PNNL/COMET/build/lib:/home/patrick/Work/PNNL/COMET/install/lib"
 #export COMET_PATH="/home/patrick/Work/PNNL/COMET/build"
 #export LLVM_PATH="/home/patrick/Work/PNNL/COMET/llvm/build/bin"
-export LD_LIBRARY_PATH="/home/pflynn5/COMET/install/lib"
+
+export LD_LIBRARY_PATH="/home/pflynn5/COMET/build/lib:/home/pflynn5/COMET/install/lib"
 export COMET_PATH="/home/pflynn5/COMET/build"
 export LLVM_PATH="/home/pflynn5/COMET/llvm/build/bin"
+
 
 ##
 ## Depends on environment variables being set
@@ -27,79 +25,86 @@ export LLVM_PATH="/home/pflynn5/COMET/llvm/build/bin"
 ## $1 = the name for our file
 ##
 function run_benchmark() {
-    # CSR
-    printf "" > csv/CSR/CSR_$1.csv
-    echo "Running $1 for CSR"
-    $COMET_PATH/bin/comet-opt --convert-ta-to-it --convert-to-loops --convert-to-llvm $1/csr_$1.mlir &> build/csr_$1.mlir
-    $LLVM_PATH/mlir-cpu-runner build/csr_$1.mlir -O3 -e main -entry-point-result=void \
-        -shared-libs=$COMET_PATH/lib/libcomet_runner_utils.so | grep -oP '\d+\.\d+' >> csv/CSR/CSR_$1.csv 
+    ###############################
+    ## CSR
+    ###############################
+    $COMET_PATH/bin/comet-opt --convert-ta-to-it --convert-to-loops --convert-to-llvm $1/csr_$1.mlir &> build/csr.mlir
+    $LLVM_PATH/mlir-translate --mlir-to-llvmir build/csr.mlir &> build/csr.ll
     
-    # COO
-    printf "" > csv/COO/COO_$1.csv
-    echo "Running $1 for COO"
-    $COMET_PATH/bin/comet-opt --convert-ta-to-it --convert-to-loops --convert-to-llvm $1/coo_$1.mlir &> build/coo_$1.mlir
-    $LLVM_PATH/mlir-cpu-runner build/coo_$1.mlir -O3 -e main -entry-point-result=void \
-        -shared-libs=$COMET_PATH/lib/libcomet_runner_utils.so | grep -oP '\d+\.\d+' >> csv/COO/COO_$1.csv
+    # Test 1
+    printf "" > csv/CSR_$1_native_O2.csv
+    $LLVM_PATH/clang build/csr.ll -o build/csr -L$COMET_PATH/lib -lcomet_runner_utils -march=native -O2
+    build/csr | grep -oP '\d+\.\d+' >> csv/CSR_$1_native_O2.csv
     
-    # ELL
-    printf "" > csv/ELL/ELL_$1.csv
-    echo "Running $1 for ELL"
-    $COMET_PATH/bin/comet-opt --convert-ta-to-it --convert-to-loops --convert-to-llvm $1/ell_$1.mlir &> build/ell_$1.mlir
-    $LLVM_PATH/mlir-cpu-runner build/ell_$1.mlir -O3 -e main -entry-point-result=void \
-        -shared-libs=$COMET_PATH/lib/libcomet_runner_utils.so | grep -oP '\d+\.\d+' >> csv/ELL/ELL_$1.csv
+    # Test 2
+    printf "" > csv/CSR_$1_native_O3.csv
+    $LLVM_PATH/clang build/csr.ll -o build/csr -L$COMET_PATH/lib -lcomet_runner_utils -march=native -O3
+    build/csr | grep -oP '\d+\.\d+' >> csv/CSR_$1_native_O3.csv
     
-    # BCSR
-    # Block sizes 2, 4, 8, 16, 32, default
-    export BLOCK_ROWS=1
-    export BLOCK_COLS=1
-    echo "Running $1 for BCSR 1x1"
-    printf "" > csv/BCSR/BCSR_$1_1x1.csv
-    $COMET_PATH/bin/comet-opt --convert-ta-to-it --convert-to-loops --convert-to-llvm $1/bcsr_$1.mlir &> build/bcsr_$1.mlir
-    $LLVM_PATH/mlir-cpu-runner build/bcsr_$1.mlir -O3 -e main -entry-point-result=void \
-        -shared-libs=$COMET_PATH/lib/libcomet_runner_utils.so | grep -oP '\d+\.\d+' >> csv/BCSR/BCSR_$1_1x1.csv
+    # Test 3
+    printf "" > csv/CSR_$1_knl_O2.csv
+    $LLVM_PATH/clang build/csr.ll -o build/csr -L$COMET_PATH/lib -lcomet_runner_utils -march=knl -O2
+    build/csr | grep -oP '\d+\.\d+' >> csv/CSR_$1_knl_O2.csv
     
-    # Block size 2
-    export BLOCK_ROWS=2
-    export BLOCK_COLS=2
-    echo "Running $1 for BCSR 2x2"
-    printf "" > csv/BCSR/BCSR_$1_2x2.csv
-    $COMET_PATH/bin/comet-opt --convert-ta-to-it --convert-to-loops --convert-to-llvm $1/bcsr_$1.mlir &> build/bcsr_$1.mlir
-    $LLVM_PATH/mlir-cpu-runner build/bcsr_$1.mlir -O3 -e main -entry-point-result=void \
-        -shared-libs=$COMET_PATH/lib/libcomet_runner_utils.so | grep -oP '\d+\.\d+' >> csv/BCSR/BCSR_$1_2x2.csv
+    # Test 4
+    printf "" > csv/CSR_$1_knl_O3.csv
+    $LLVM_PATH/clang build/csr.ll -o build/csr -L$COMET_PATH/lib -lcomet_runner_utils -march=knl -O3
+    build/csr | grep -oP '\d+\.\d+' >> csv/CSR_$1_knl_O3.csv
     
-    # Block size 4
-    export BLOCK_ROWS=4
-    export BLOCK_COLS=4
-    echo "Running $1 for BCSR 4x4"
-    printf "" > csv/BCSR/BCSR_$1_4x4.csv
-    $COMET_PATH/bin/comet-opt --convert-ta-to-it --convert-to-loops --convert-to-llvm $1/bcsr_$1.mlir &> build/bcsr_$1.mlir
-    $LLVM_PATH/mlir-cpu-runner build/bcsr_$1.mlir -O3 -e main -entry-point-result=void \
-        -shared-libs=$COMET_PATH/lib/libcomet_runner_utils.so | grep -oP '\d+\.\d+' >> csv/BCSR/BCSR_$1_4x4.csv
-        
-    # Block size 16
-    export BLOCK_ROWS=16
-    export BLOCK_COLS=16
-    echo "Running $1 for BCSR 16x16"
-    printf "" > csv/BCSR/BCSR_$1_16x16.csv
-    $COMET_PATH/bin/comet-opt --convert-ta-to-it --convert-to-loops --convert-to-llvm $1/bcsr_$1.mlir &> build/bcsr_$1.mlir
-    $LLVM_PATH/mlir-cpu-runner build/bcsr_$1.mlir -O3 -e main -entry-point-result=void \
-        -shared-libs=$COMET_PATH/lib/libcomet_runner_utils.so | grep -oP '\d+\.\d+' >> csv/BCSR/BCSR_$1_16x16.csv
+    ###############################
+    ## ELL
+    ###############################
+    $COMET_PATH/bin/comet-opt --convert-ta-to-it --convert-to-loops --convert-to-llvm $1/ell_$1.mlir &> build/ell.mlir
+    $LLVM_PATH/mlir-translate --mlir-to-llvmir build/ell.mlir &> build/ell.ll
+    
+    # Test 1
+    printf "" > csv/ELL_$1_native_O2.csv
+    $LLVM_PATH/clang build/ell.ll -o build/ell -L$COMET_PATH/lib -lcomet_runner_utils -march=native -O2
+    build/ell | grep -oP '\d+\.\d+' >> csv/ELL_$1_native_O2.csv
+    
+    # Test 2
+    printf "" > csv/ELL_$1_native_O3.csv
+    $LLVM_PATH/clang build/ell.ll -o build/ell -L$COMET_PATH/lib -lcomet_runner_utils -march=native -O3
+    build/ell | grep -oP '\d+\.\d+' >> csv/ELL_$1_native_O3.csv
+    
+    # Test 3
+    printf "" > csv/ELL_$1_knl_O2.csv
+    $LLVM_PATH/clang build/ell.ll -o build/ell -L$COMET_PATH/lib -lcomet_runner_utils -march=knl -O2
+    build/ell | grep -oP '\d+\.\d+' >> csv/ELL_$1_knl_O2.csv
+    
+    # Test 4
+    printf "" > csv/ELL_$1_knl_O3.csv
+    $LLVM_PATH/clang build/ell.ll -o build/ell -L$COMET_PATH/lib -lcomet_runner_utils -march=knl -O3
+    build/ell | grep -oP '\d+\.\d+' >> csv/ELL_$1_knl_O3.csv
 }
 
-#export SPARSE_FILE_NAME0=../data/data/bcsstk17/bcsstk17.mtx
-#run_benchmark "bcsstk17"
+export SPARSE_FILE_NAME0=../data/data/bcsstk17/bcsstk17.mtx
+run_benchmark "bcsstk17"
 
-#export SPARSE_FILE_NAME0=../data/data/cant/cant.mtx
-#run_benchmark "cant"
+export SPARSE_FILE_NAME0=../data/data/cant/cant.mtx
+run_benchmark "cant"
 
-# BCSR 16x16 needed
-#export SPARSE_FILE_NAME0=../data/data/consph/consph.mtx
-#run_benchmark "consph"
+export SPARSE_FILE_NAME0=../data/data/consph/consph.mtx
+run_benchmark "consph"
 
-#export SPARSE_FILE_NAME0=../data/data/cop20k_A/cop20k_A.mtx
-#run_benchmark "cop20k_A"
+export SPARSE_FILE_NAME0=../data/data/cop20k_A/cop20k_A.mtx
+run_benchmark "cop20k_A"
 
 export SPARSE_FILE_NAME0=../data/data/pdb1HYS/pdb1HYS.mtx
 run_benchmark "pdb1HYS"
 
+export SPARSE_FILE_NAME0=../data/data/rma10/rma10.mtx
+run_benchmark "rma10"
+
+export SPARSE_FILE_NAME0=../data/data/rma10/rma10_b.mtx
+run_benchmark "rma10_b"
+
+export SPARSE_FILE_NAME0=../data/data/shipsec1/shipsec1.mtx
+run_benchmark "shipsec1"
+
+export SPARSE_FILE_NAME0=../data/data/scircuit/scircuit.mtx
+run_benchmark "scircuit"
+
+export SPARSE_FILE_NAME0=../data/data/scircuit/scircuit_b.mtx
+run_benchmark "scircuit_b"
 
