0e42d3ba22ba1694f37ae1384ffc247a3e04d628
Patrick Flynn <pflynn157@gmail.com>
2024-01-16 18:32:06 -0500

    Vectorization with ELLPACK

--- ellpack2/ell-c/ell.c
+++ ellpack2/ell-c/ell.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sys/time.h>
+#include <immintrin.h>
 
 double getTime()
 {
@@ -35,29 +36,85 @@ int main(int argc, char **argv) {
     double *values = malloc(sizeof(double)*vals_size);
     
     double *vector = malloc(sizeof(double)*rows);
-    double *results = malloc(sizeof(double)*rows);
+    double *results1 = malloc(sizeof(double)*rows);
+    double *results2 = malloc(sizeof(double)*rows);
+    double *results3 = malloc(sizeof(double)*rows);
     for (size_t i = 0; i<rows; i++) {
         vector[i] = 1.7;
-        results[i] = 0;
+        results1[i] = 0;
+        results2[i] = 0;
+        results3[i] = 0;
     }
     
     for (size_t i = 0; i<columnIdxs_size; i++) columnIdxs[i] = v_columnIdxs[i];
     for (size_t i = 0; i<vals_size; i++) values[i] = v_values[i];
 
-    // Measure time
+    // Measure time (algorithm 1)
     double stime = getTime();
-    //for (uint64_t i = 0; i<rows; i++) {
-    //    for (uint64_t n1 = 0; n1<cols; n1++) {
-    for (uint64_t n1 = 0; n1<cols; n1++) {   
-        #pragma omp simd 
+    for (uint64_t n1 = 0; n1<cols; n1++) {
         for (uint64_t i = 0; i<rows; i++) {
-            results[i] += values[n1 * rows + i] * vector[columnIdxs[n1 * rows + i]];
+            results1[i] += values[n1 * rows + i] * vector[columnIdxs[n1 * rows + i]];
         }
     }
     double etime = getTime();
+    printf("Serial: ");
     printElapsedTime(stime, etime);
     
-    for (size_t i = 0; i<rows; i++) fprintf(stderr, "%.1f,", results[i]);
+    // Algorithm 2
+    stime = getTime();
+    //__mmask16 __mask2 = _mm512_kmov(0xFFFF);
+    __mmask8 __mask = _mm512_int2mask(0xFF);
+    __m512d buf0 = _mm512_setzero_pd();
+    
+    for (uint64_t n1 = 0; n1<cols; n1++) {
+        for (uint64_t i = 0; i<rows; i+=8) {
+            //results2[i] += values[n1 * rows + i] * vector[columnIdxs[n1 * rows + i]];
+            
+            __m512i __vindex0 = _mm512_loadu_si512((__m512i *)(&columnIdxs[n1 * rows + i]));
+            //__m512d vec_vector = _mm512_i64gather_pd(__vindex0, vector, 8);
+            __m512d vec_vector = _mm512_mask_i64gather_pd(buf0, __mask, __vindex0, vector, 8);
+            
+            __m512d vec1 = _mm512_loadu_pd(&results2[i]);
+            __m512d vec_values = _mm512_loadu_pd(&values[n1 * rows + i]);
+            
+            __m512d vec2 = _mm512_mul_pd(vec_values, vec_vector);
+            __m512d vec3 = _mm512_add_pd(vec1, vec2);
+            
+            _mm512_storeu_pd(&results2[i], vec3);
+        }
+    }
+    etime = getTime();
+    printf("Vector: ");
+    printElapsedTime(stime, etime);
+    
+    // Algorithm 3
+    stime = getTime();
+    for (uint64_t n1 = 0; n1<cols; n1++) {
+        #pragma omp simd
+        for (uint64_t i = 0; i<rows; i++) {
+            results3[i] += values[n1 * rows + i] * vector[columnIdxs[n1 * rows + i]];
+        }
+    }
+    etime = getTime();
+    printf("OpenMP: ");
+    printElapsedTime(stime, etime);
+    
+    puts("========================");
+    
+    // Results 1
+    for (size_t i = 0; i<30; i+=3) fprintf(stderr, "%.0lf,", results1[i]);
+    fprintf(stderr, "\n");
+    
+    puts("------------------------");
+    
+    // Results 2
+    for (size_t i = 0; i<30; i+=3) fprintf(stderr, "%.0lf,", results2[i]);
+    fprintf(stderr, "\n");
+    
+    puts("------------------------");
+    
+    // Results 3
+    for (size_t i = 0; i<30; i+=3) fprintf(stderr, "%.0lf,", results3[i]);
     fprintf(stderr, "\n");
     
     return 0;
