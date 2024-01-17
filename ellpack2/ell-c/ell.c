74d70708a9aee04eb3e390d65b51f92b96dce134
Patrick Flynn <pflynn157@gmail.com>
2024-01-17 11:07:41 -0500

    Added ELL testing

--- ellpack2/ell-c/ell.c
+++ ellpack2/ell-c/ell.c
@@ -1,87 +1,39 @@
-// run:
-// gcc test2.cpp -o test2 -march=native -O3 -fopenmp && ./test2 2> actual.txt && diff actual.txt expected.txt
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <sys/time.h>
-#include <immintrin.h>
+#include <stdint.h>
 
-double getTime()
-{
-  struct timeval tp;
-  int stat = gettimeofday(&tp, NULL);
-  return (tp.tv_sec + tp.tv_usec * 1.0e-6);
-}
-
-void printElapsedTime(double stime, double etime)
-{
-  fprintf(stdout, "ELAPSED_TIME = %lf\n", etime - stime);
-}
-
-int main(int argc, char **argv) {
-    #ifdef SQUARE
-    uint64_t rows = 512;
-    uint64_t cols = 32;
-    #else
-    #ifdef BCSSTK17
-    uint64_t rows = 10974;
-    uint64_t cols = 150;
-    #endif
-    #endif
+//@ell_test1(i64, i64, !llvm.ptr<i8>,   //columnPtr
+//                i64, !llvm.ptr<i8>,   // columnIdx
+//                i64, !llvm.ptr<i8>,   // values
+//                i64, !llvm.ptr<i8>,   // B
+//                i64, !llvm.ptr<i8>)   // C
+void ell_test1(int64_t rows, int64_t cols,
+                int64_t columnIdxs_len, void **_columnIdxs,
+                int64_t values_len, void **_values,
+                int64_t B_len, void **_B,
+                int64_t C_len, void **_C) {
+    int64_t *columnIdxs = _columnIdxs[0];
+    double *values = _values[0];
+    double *B = _B[1];
+    double *C = _C[1];
     
-    uint64_t v_columnIdxs[] = {
-        #ifdef SQUARE
-        #include "square/cols.txt"
-        #else
-        #ifdef BCSSTK17
-        #include "bcsstk17/cols.txt"
-        #endif
-        #endif
-    };
-    double v_values[] = {
-        #ifdef SQUARE
-        #include "square/vals.txt"
-        #ifdef BCSSTK17
-        #include "bcsstk17/vals.txt"
-        #endif
-        #endif
-    };
-    
-    size_t columnIdxs_size = sizeof(v_columnIdxs) / sizeof(uint64_t);
-    size_t vals_size = sizeof(v_values) / sizeof(double);
-    
-    uint64_t *columnIdxs = malloc(sizeof(uint64_t)*columnIdxs_size);
-    double *values = malloc(sizeof(double)*vals_size);
-    
-    double *vector = malloc(sizeof(double)*rows);
-    double *results1 = malloc(sizeof(double)*rows);
-    double *results2 = malloc(sizeof(double)*rows);
-    double *results3 = malloc(sizeof(double)*rows);
-    for (size_t i = 0; i<rows; i++) {
-        vector[i] = 1.7;
-        results1[i] = 0;
-        results2[i] = 0;
-        results3[i] = 0;
-    }
-    
-    for (size_t i = 0; i<columnIdxs_size; i++) columnIdxs[i] = v_columnIdxs[i];
-    for (size_t i = 0; i<vals_size; i++) values[i] = v_values[i];
-
-    // Measure time (algorithm 1)
-    double stime = getTime();
     for (uint64_t n1 = 0; n1<cols; n1++) {
         for (uint64_t i = 0; i<rows; i++) {
-            results1[i] += values[n1 * rows + i] * vector[columnIdxs[n1 * rows + i]];
+            C[i] += values[n1 * rows + i] * B[columnIdxs[n1 * rows + i]];
         }
     }
-    double etime = getTime();
-    printf("Serial: ");
-    printElapsedTime(stime, etime);
+}
+
+// Vectorization test
+void ell_test2(int64_t rows, int64_t cols,
+                int64_t columnIdxs_len, void **_columnIdxs,
+                int64_t values_len, void **_values,
+                int64_t B_len, void **_B,
+                int64_t C_len, void **_C) {
+    int64_t *columnIdxs = _columnIdxs[0];
+    double *values = _values[0];
+    double *B = _B[1];
+    double *C = _C[1];
     
-    // Algorithm 2
-    stime = getTime();
-    //__mmask16 __mask2 = _mm512_kmov(0xFFFF);
     __mmask8 __mask = _mm512_int2mask(0xFF);
     __m512d buf0 = _mm512_setzero_pd();
     
@@ -91,51 +43,16 @@ int main(int argc, char **argv) {
             
             __m512i __vindex0 = _mm512_loadu_si512((__m512i *)(&columnIdxs[n1 * rows + i]));
             //__m512d vec_vector = _mm512_i64gather_pd(__vindex0, vector, 8);
-            __m512d vec_vector = _mm512_mask_i64gather_pd(buf0, __mask, __vindex0, vector, 8);
+            __m512d vec_vector = _mm512_mask_i64gather_pd(buf0, __mask, __vindex0, B, 8);
             
-            __m512d vec1 = _mm512_loadu_pd(&results2[i]);
+            __m512d vec1 = _mm512_loadu_pd(&C[i]);
             __m512d vec_values = _mm512_loadu_pd(&values[n1 * rows + i]);
             
             __m512d vec2 = _mm512_mul_pd(vec_values, vec_vector);
             __m512d vec3 = _mm512_add_pd(vec1, vec2);
             
-            _mm512_storeu_pd(&results2[i], vec3);
+            _mm512_storeu_pd(&C[i], vec3);
         }
     }
-    etime = getTime();
-    printf("Vector: ");
-    printElapsedTime(stime, etime);
-    
-    // Algorithm 3
-    stime = getTime();
-    for (uint64_t n1 = 0; n1<cols; n1++) {
-        #pragma omp simd
-        for (uint64_t i = 0; i<rows; i++) {
-            results3[i] += values[n1 * rows + i] * vector[columnIdxs[n1 * rows + i]];
-        }
-    }
-    etime = getTime();
-    printf("OpenMP: ");
-    printElapsedTime(stime, etime);
-    
-    puts("========================");
-    
-    // Results 1
-    for (size_t i = 0; i<30; i+=3) fprintf(stderr, "%.0lf,", results1[i]);
-    fprintf(stderr, "\n");
-    
-    puts("------------------------");
-    
-    // Results 2
-    for (size_t i = 0; i<30; i+=3) fprintf(stderr, "%.0lf,", results2[i]);
-    fprintf(stderr, "\n");
-    
-    puts("------------------------");
-    
-    // Results 3
-    for (size_t i = 0; i<30; i+=3) fprintf(stderr, "%.0lf,", results3[i]);
-    fprintf(stderr, "\n");
-    
-    return 0;
 }
 
--- ellpack2/ell-c/run.sh
+++ ellpack2/ell-c/run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+#####################################3
+# Testing
+export LD_LIBRARY_PATH="/home/patrick/Work/PNNL/COMET/install/lib:/home/patrick/Work/PNNL/COMET/build/lib"
+export SPARSE_FILE_NAME0=small.mtx
 
-clang ell.c -o ell -march=native -O2 -DSQUARE
-./ell #2> actual.txt
-#diff actual.txt expected.txt
+export COMET_BASE="/home/patrick/Work/PNNL/COMET/build"
+export LLVM_BASE="/home/patrick/Work/PNNL/COMET/llvm/build/bin"
 
-clang ell.c -o ell -march=native -O2 -DBCSSTK17
+$COMET_BASE/bin/comet-opt --convert-ta-to-it --convert-to-loops --convert-to-llvm ell.mlir &> /tmp/first.mlir
+$LLVM_BASE/mlir-translate --mlir-to-llvmir /tmp/first.mlir &> /tmp/first.ll
+
+$LLVM_BASE/clang /tmp/first.ll ell.c -o ell -L$COMET_BASE/lib -lcomet_runner_utils -march=native -O2 -ftree-vectorize
 ./ell
+
 
