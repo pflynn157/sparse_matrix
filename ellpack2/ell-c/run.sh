f85ec1d3cebaafdc24ac6d6409499072beb3171f
Patrick Flynn <pflynn157@gmail.com>
2024-01-17 11:12:44 -0500

    Updated scripts

--- ellpack2/ell-c/run.sh
+++ ellpack2/ell-c/run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #####################################3
 # Testing
-export LD_LIBRARY_PATH="/home/patrick/Work/PNNL/COMET/install/lib:/home/patrick/Work/PNNL/COMET/build/lib"
-export SPARSE_FILE_NAME0=small.mtx
+#export LD_LIBRARY_PATH="/home/patrick/Work/PNNL/COMET/install/lib:/home/patrick/Work/PNNL/COMET/build/lib"
+#export COMET_BASE="/home/patrick/Work/PNNL/COMET/build"
+#export LLVM_BASE="/home/patrick/Work/PNNL/COMET/llvm/build/bin"
 
-export COMET_BASE="/home/patrick/Work/PNNL/COMET/build"
-export LLVM_BASE="/home/patrick/Work/PNNL/COMET/llvm/build/bin"
+export LD_LIBRARY_PATH="/home/pflynn5/COMET/build/lib:/home/pflynn5/COMET/install/lib"
+export COMET_PATH="/home/pflynn5/COMET/build"
+export LLVM_PATH="/home/pflynn5/COMET/llvm/build/bin"
+
+export SPARSE_FILE_NAME0=small.mtx
 
 $COMET_BASE/bin/comet-opt --convert-ta-to-it --convert-to-loops --convert-to-llvm ell.mlir &> /tmp/first.mlir
 $LLVM_BASE/mlir-translate --mlir-to-llvmir /tmp/first.mlir &> /tmp/first.ll
