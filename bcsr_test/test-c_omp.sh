97bc19dd7297b1bcf02a45a55718090519ad7b6a
Patrick Flynn <pflynn157@gmail.com>
2024-01-31 13:15:06 -0500

    Fix to OMP programs

--- bcsr_test/test-c_omp.sh
+++ bcsr_test/test-c_omp.sh
@@ -2,14 +2,16 @@
 #####################################3
 # Testing
 # Work PC
-#export LD_LIBRARY_PATH="/home/pflynn5/PNNL/COMET/install/lib:/home/pflynn5/PNNL/COMET/build/lib"
+#export LD_LIBRARY_PATH="/home/pflynn5/PNNL/COMET/install/lib:/home/pflynn5/PNNL/COMET/build/lib:/home/pflynn5/PNNL/COMET/llvm/build/lib"
 #export COMET_BASE="/home/pflynn5/PNNL/COMET/build"
 #export LLVM_BASE="/home/pflynn5/PNNL/COMET/llvm/build/bin"
+#export LLVM_LIB="/home/pflynn5/PNNL/COMET/llvm/build/lib"
 
 # Carina
 export LD_LIBRARY_PATH="/home/pflynn5/COMET/build/lib:/home/pflynn5/COMET/install/lib"
 export COMET_BASE="/home/pflynn5/COMET/build"
 export LLVM_BASE="/home/pflynn5/COMET/llvm/build/bin"
+export LLVM_LIB="/home/pflynn5/COMET/llvm/build/lib"
 
 # PC
 #export LD_LIBRARY_PATH="/home/patrick/Work/PNNL/COMET/build/lib:/home/patrick/Work/PNNL/COMET/install/lib"
@@ -60,7 +62,8 @@ function run() {
             $LLVM_BASE/mlir-translate --mlir-to-llvmir /tmp/first.mlir &> /tmp/first.ll
             $LLVM_BASE/clang mlir-c-omp/lib.c -c -o /tmp/lib.o -march=native -O2 -ftree-vectorize -fopenmp
 
-            $LLVM_BASE/clang /tmp/first.ll /tmp/lib.o -o exe -L$COMET_BASE/lib -lcomet_runner_utils -O2
+            $LLVM_BASE/clang /tmp/first.ll /tmp/lib.o -o exe -L$COMET_BASE/lib -lcomet_runner_utils -O2 \
+                -L$LLVM_LIB -lomp
             printf "" > csv/$2/$1_$3_"$b"x"$b".csv
             ./exe  | grep -oP '\d+\.\d+' >> csv/$2/$1_$3_"$b"x"$b".csv
             #./exe
